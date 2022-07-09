5ad1b81becb9164e6e8f6e81e3d0fd262089103d
Maciej Sakowski <msakow@gmail.com>
2022-07-09 14:45:10 +0200

    Select browser for testing

--- addressbook-tests/src/test/java/web/tests/addressbook/appmanager/ApplicationManager.java
+++ addressbook-tests/src/test/java/web/tests/addressbook/appmanager/ApplicationManager.java
@@ -1,11 +1,19 @@
 package web.tests.addressbook.appmanager;
 
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.remote.Browser;
 
 import java.util.concurrent.TimeUnit;
 
 public class ApplicationManager {
+  private Browser browser;
+
+  public ApplicationManager(Browser browser) {
+    this.browser = browser;
+  }
+
   private ContactHelper contactHelper;
   private NavigationHelper navigationHelper;
   public WebDriver wd;
@@ -13,7 +21,12 @@ public class ApplicationManager {
   private SessionHelper sessionHelper;
 
   public void init() {
-    wd = new FirefoxDriver();
+    if (browser .equals(Browser.FIREFOX)) {
+      wd = new FirefoxDriver();
+    } else if (browser .equals(Browser.CHROME)) {
+      wd = new ChromeDriver();
+    }
+
     wd.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
     groupHelper = new GroupHelper(wd);
     navigationHelper = new NavigationHelper(wd);
--- addressbook-tests/src/test/java/web/tests/addressbook/tests/TestBase.java
+++ addressbook-tests/src/test/java/web/tests/addressbook/tests/TestBase.java
@@ -1,12 +1,13 @@
 package web.tests.addressbook.tests;
 
+import org.openqa.selenium.remote.Browser;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import web.tests.addressbook.appmanager.ApplicationManager;
 
 public class TestBase {
 
-  public final ApplicationManager app = new ApplicationManager();
+  public final ApplicationManager app = new ApplicationManager(Browser.CHROME);
 
   @BeforeMethod(alwaysRun = true)
   public void setUp() throws Exception {
